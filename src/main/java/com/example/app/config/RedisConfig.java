e348d04b0480c880ec8d7170604ce91a10b676fc
mertcakmak2 <mertcakmak2@gmail.com>
2021-11-26 13:25:01 +0300

    redis deployment files, redis config

---
+++ src/main/java/com/example/app/config/RedisConfig.java
@@ -0,0 +1,37 @@
+package com.example.app.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+
+    @Bean
+    public JedisConnectionFactory connectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName("167.172.105.147");
+        configuration.setPort(32666);
+        return new JedisConnectionFactory(configuration);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> template() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new JdkSerializationRedisSerializer());
+        template.setValueSerializer(new JdkSerializationRedisSerializer());
+        template.setEnableTransactionSupport(true);
+        template.afterPropertiesSet();
+        return template;
+    }
+}
--- src/main/java/com/example/app/controller/EmployeeController.java
+++ src/main/java/com/example/app/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.example.app.controller;
 import com.example.app.model.Employee;
 import com.example.app.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,7 +16,9 @@ public class EmployeeController {
     private final EmployeeRepository employeeRepository;
 
     @GetMapping(value = "")
+    @Cacheable(cacheNames = "employee_list")
     public List<Employee> findAll(){
+        System.out.println("fetch to db");
         return employeeRepository.findAll();
     }
 
